523a3828b2ea997b60e5526d845c213110335503
maxpaynecoool <goncharov789@gmail.com>
2023-04-14 09:38:06 +0300

    final commit

--- HW1/index.js
+++ HW1/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+var v1 = 42;
+var v2 = 'Сорок два';
+var v3 = true;
+var v4 = null;
+
+alert('Добро пожаловать!');
+alert('У меня есть 4 переменных с разными типами данных');
+alert(`Переменная: ${v1}`);
+alert('Это переменная типа number');
+alert(`Переменная: ${v2}`);
+alert('Это переменная типа string');
+alert(`Переменная: ${v3}`);
+alert('Это переменная типа boolean');
+alert(`Переменная: ${v4}`);
+alert('Это переменная типа null (неожиданно)');
+
+var enjoy = confirm('Понравился ли вам сайт');
+
+alert(enjoy);
+
+var realyEnjoy = prompt('Точно понравился?')
+
+alert(realyEnjoy);
+
+console.log(enjoy);
+console.log(realyEnjoy);
