9b65b40d121eb1c65218d8865ee594eb5338770e
maxpaynecoool <goncharov789@gmail.com>
2023-05-07 14:26:40 +0300

    sixth homework

--- HW6/index.js
+++ HW6/index.js
@@ -1,10 +1,3 @@
-/*let defaultString = 'ТуТ я пиШУ ТЕСтоВУю СТРОКУ'
-
-for (let i = 0; i < defaultString.length; i++) {
-  if (defaultString[0]) continue
-  console.log(defaultString[i])
-}*/
-
 const everyWordToUpperCase = (text) => {
   let str = '';
   for (let i = 0; i <= text.length - 1; i++) {
