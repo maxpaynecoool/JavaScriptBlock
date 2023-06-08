8f69fa1a2cbb39eb34b7eb53c2c1ed7b7d1a9879
maxpaynecoool <goncharov789@gmail.com>
2023-06-08 23:51:14 +0300

    friends

--- HW11-Friends/index.js
+++ HW11-Friends/index.js
@@ -3,33 +3,40 @@ const inputFriend = document.getElementById('inputFriend')
 const addBtn = document.getElementById('addBtn')
 const friendsList = document.getElementById('friendsList')
 const emptyInput = document.getElementById('emptyInput')
+const deleteBtn = document.getElementById('deleteBtn')
 
-for(let i = 1; i <= localStorage.length; i++) {
-  let item = localStorage.getItem(i)
-
-  let lili = document.createElement('li')
-  lili.textContent = item
-  friendsList.append(lili)
+const addLi = (liContent) => {
+  let li = document.createElement('li')
+  li.textContent = liContent
+  friendsList.append(li)
   countNumber.textContent++
+  localStorage.setItem(countNumber.textContent, liContent)
+  inputFriend.value = null
+}
+
+for (let i = 1; i <= localStorage.length; i++) {
+  let item = localStorage.getItem(i)
+  addLi(item)
 }
 
 addBtn.onclick = () => {
   if (inputFriend.value === '') {
     emptyInput.hidden = false;
+    emptyInput.textContent = 'Введите друга'
     return
-  } 
+  }
+  addLi(inputFriend.value)
+}
 
-  let li = document.createElement('li')
-
-  li.textContent = inputFriend.value
-
-  friendsList.append(li)
-
-  countNumber.textContent++
-
-  localStorage.setItem(countNumber.textContent, inputFriend.value)
-
-  inputFriend.value = null
+deleteBtn.onclick = () => {
+  if (countNumber.textContent !== '0') {
+    friendsList.lastChild.remove()
+    localStorage.removeItem(countNumber.textContent)
+    countNumber.textContent--
+  } else {
+    emptyInput.hidden = false;
+    emptyInput.textContent = 'Нечего удалять'
+  }
 }
 
 inputFriend.oninput = () => {
